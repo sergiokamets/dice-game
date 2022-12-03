8c28682b5a4ea79ce229b5eeb6bfdffc0563c88a
Serhii Kamets <sergiokamets@gmail.com>
2022-12-03 15:11:53 +0100

    Changed goal point score

--- js/script.js
+++ js/script.js
@@ -78,7 +78,7 @@ btnHold.addEventListener('click', function () {
     scores[activePlayer];
 
   // 2. Check if player score is >= 100
-  if (scores[activePlayer] >= 10) {
+  if (scores[activePlayer] >= 100) {
     playing = false;
     diceEl.classList.add('hidden');
     document.getElementById('players').classList.add('player--winners');
